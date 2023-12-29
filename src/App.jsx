0de74b6b9db67ce11e9a3f17f0d5ca62bbbf238b
Dominic Buchheit <dominic.buchheit@gmail.com>
2023-12-29 10:30:50 +0100

    Adding a dedicated home page as the only page for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { DarkModeProvider } from "./contexts/DarkModeContext";
 import GlobalStyles from "./styles/GlobalStyles";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <DarkModeProvider>
       <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="home" />} />
+          <Route path="home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </DarkModeProvider>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledHome = styled.main`
+  margin: 5rem;
+`;
+
+function Home() {
+  return <StyledHome>Home Page</StyledHome>;
+}
+
+export default Home;
